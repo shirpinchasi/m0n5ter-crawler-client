e17d3b43ca88a01bec9aabcbf120362c398ded79
Shir Pinchasi <you@example.com>
2020-10-26 20:21:32 +0200

    commit 7

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.scss';
 import "bootstrap";
 
-export default class App extends Component{
+export default class Config extends Component{
     constructor(props){
       super(props);
       this.state = {
@@ -33,67 +33,55 @@ render(){
   
   return(
           
-        <div>
-          <div>
-            </div>
-          {isLoading ? (
-            <div className="cssload-tetrominos">
-              <div className="cssload-tetromino cssload-box1"></div>
-              <div className="cssload-tetromino cssload-box2"></div>
-              <div className="cssload-tetromino cssload-box3"></div>
-              <div className="cssload-tetromino cssload-box4"></div>
-            </div>
-            ) :(
-              
             <div>
-                
-            {
-              data.map((article)=>(
-                <div className = "articles">
-                        <div className ="article_Title"> Title: {article.title}</div>
-                        <div className ="article_Date">Date: {article.date}</div>
-                        <a href ={"" + article.url} className ="article_Url" target="_blank" rel="noopener noreferrer"> url link: {article.url}</a><br/>
-                        <a href = {article._links.self.href} className="article_linksHref" target="_blank"  rel="noopener noreferrer">  href :{article._links.self.href}</a><br/>
-                        <a href = {article._links.article.href} className="article_Href" target="_blank"  rel="noopener noreferrer"> article href :{article._links.article.href}</a><br/>
-                        <a href = {article._links.groups.href} className="article_GroupsHref" target="_blank"  rel="noopener noreferrer"> group href :{article._links.groups.href}</a>
-                        
+                {isLoading ? (
+                  <div className="cssload-tetrominos">
+                    <div className="cssload-tetromino cssload-box1"></div>
+                    <div className="cssload-tetromino cssload-box2"></div>
+                    <div className="cssload-tetromino cssload-box3"></div>
+                    <div className="cssload-tetromino cssload-box4"></div>
+                  </div>
+                  ) :(
+                    
+            <div>      
+            {data.map((article)=>(
+                <div id ="cards" className="col-12 col-md-5 ">
+                <div id = "articles" className="col">
+                  <div id = "backgroundTitle">
+                        <div id ="article_Title" className="row"> {article.title}</div>
+                        </div>
+                        <div id ="article_Date" className="card-subtitle mb-2 text-muted">Date: {article.date}</div>
                         <div className ="groups">
                           {article.groups.map((gr)=>(
-                            
-                                  <div className ="group_Id"> group id:{gr.id}
-                                      <div className ="group_Name">group name : {gr.name}</div>
-                                      <div className ="group_Desc">group description : {gr.description}</div>
-                                      <div className ="group_Aliases">group aliases : {gr.aliases}</div>
-                                      <div className ="group_Last_Scan">group last scan : {gr.lastScan}</div>
+                            <div id="groups">
+                                  <div id ="group_Id" className=""> group id: {gr.id}</div>
+                                  <div id ="group_Name" className="">group name:  {gr.name}</div>
+                                      {/* <div id ="group_Desc" className="card-body">group description : {gr.description}</div> */}
+                                      <div id ="group_Aliases" className="card-body">group aliases:  {gr.aliases}</div>
+                                      {/* <div id ="group_Last_Scan" className="card-body">group last scan : {gr.lastScan}</div> */}
+                                  
                                   </div>
+                                  
               ))}
+              {/* <div className="card">
+                        <a href ={"" + article.url} id ="article_Url" target="_blank" rel="noopener noreferrer" className="card link"> website link</a><br/>
+                        <a href = {article._links.self.href} id="article_linksHref" target="_blank"  rel="noopener noreferrer" className="card link">  json link</a><br/>
+                        <a href = {article._links.article.href} id="article_Href" target="_blank"  rel="noopener noreferrer" className="card link"> db link</a><br/>
+                        <a href = {article._links.groups.href} id="article_GroupsHref" target="_blank"  rel="noopener noreferrer" className="card link"> group db link</a>
+                        
+                        </div> */}
+              </div>
               </div>
               </div>
               
               ))
-            }
-                <div >
-                    <div>
-                    {/* {
-                      this.state.data.map((obj, i )=> 
-                        <div key = {i}>
-                          {obj}
-                        </div>
-                      )
-              
-                    }) */}
-                    <div>
-              
-                      </div>           
-          
-                      </div>
-                </div>
-                <div>
-                </div>
+            }  
             </div>
-            )
+            
+            )   
           }
         </div>
+        
 
         
         )
