6e7d18ee51ad739c9ba6fb8e382d4aebe5519834
Shir Pinchasi <you@example.com>
2020-11-27 19:06:08 +0200

    commit5

--- src/Groups.js
+++ src/Groups.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import './App.scss';
 import "bootstrap";
-import Groups from "./Groups"
 
 
 
-export default class App extends Component{
+
+export default class Groups extends Component{
     constructor(props){
       super(props);
       this.state = {
           data : [],
-          isLoading : true,
-          filterDataName : "",
-          filterDataTitle : ""
         
     }; 
  };
@@ -26,78 +23,48 @@ export default class App extends Component{
         this.setState({
            data :res._embedded.articles,
            isLoading : false,
-           filterDataName: "",
-           filterDataTitle : ""
+           
         })
       })
       .catch((err =>{
         console.error(err);
       }));
-      
     };
-     
 
 render(){
-  const {isLoading , data} = (this.state);
+  const {data} = (this.state);
   
   return(
-          
-            <div id="background">
-                {isLoading ? (
-                  <div className="cssload-tetrominos">
-                    <div className="cssload-tetromino cssload-box1"></div>
-                    <div className="cssload-tetromino cssload-box2"></div>
-                    <div className="cssload-tetromino cssload-box3"></div>
-                    <div className="cssload-tetromino cssload-box4"></div>
-                  </div>
-                  ) :(
-                    
-            <div className=" bg-dark text-white">   
-              <div className ="navbar navbar-expand-lg mt-2 ">
-              <a className="navbar-brand " href="#">M0n5ter Crawler</a>
-              
-              
-              {/* <a href="config" className="nav-link">groups</a> */}
-             
+<div>   
     <input type="checkbox" id="check"/>
     <label htmlFor="check">
       <i className="fa fa-bars" id="btn"></i>
       <i className="fa fa-times" id="cancel"></i>
     </label>
     <div className="sidebar">
-    <input className="input" name="filterData"type="text" onChange={this.handleChange}/>
+        <input className="input" type="text" />
     
-  <ul>
-<li><a href= "/" alt ="Groups"><i className="fa fa-users"></i>Groups</a></li>
-<li><a href="#"><i className="fa fa-info"></i>About</a></li>
-<li><a href="#"><i className="fas fa-stream"></i>Overview</a></li>
-<li><a href="#"><i className="fas fa-calendar-week"></i>Events</a></li>
-<li><a href="#"><i className="far fa-question-circle"></i>About</a></li>
-<li><a href="#"><i className="fas fa-sliders-h"></i>Services</a></li>
-<li><a href="#"><i className="far fa-envelope"></i>Contact</a></li>
-</ul>
-</div>
-</div>
+    <ul>
+        <li><a href="#"><i className="fa fa-users"></i>Groups</a></li>
+        <li><a href="#"><i className="fa fa-info"></i>About</a></li>
+        <li><a href="#"><i className="fas fa-stream"></i>Overview</a></li>
+        <li><a href="#"><i className="fas fa-calendar-week"></i>Events</a></li>
+        <li><a href="#"><i className="far fa-question-circle"></i>About</a></li>
+        <li><a href="#"><i className="fas fa-sliders-h"></i>Services</a></li>
+        <li><a href="#"><i className="far fa-envelope"></i>Contact</a></li>
+    </ul>
+    </div>
             <div id="bg-dark"> 
            
             {data.map((article)=>(
-              
               <div>
-                
-                <div id ="cards" className="col-12 col-md- col-sm-2 col-xs">
-                <div id = "articles" className="col">
-                  <div id = "backgroundTitle">
-                        <div key={article.title} id ="article_Title" className="row"> {article.title}</div>
-                        </div>
-                        <div key={article.date} id ="article_Date" className="card-subtitle mb-2 text-muted">{article.date}</div>
-                        <div className ="groups">
                           {article.groups.map((gr)=>(
                             <div id="groups">
                               <div className="badge badge-success">
                                 <div key={gr.name} id ="group_Name" className="">{gr.name}
                                 
                                   {/* <div id ="group_Id" className=""> ID: {gr.id} aliases:  {gr.aliases}</div> */}
-                                  <div id ="group_Aliases" className="card-body"></div>
+                                  {/* <div id ="group_Aliases" className="card-body"></div> */}
                                   </div>
                                       {/* <div id ="group_Desc" className="card-body">group description : {gr.description}</div> 
                                       
@@ -116,9 +83,6 @@ render(){
                         </div> */}
               
               </div>
-              </div>
-              </div>
-              </div>
               
               
               ))
@@ -128,12 +92,7 @@ render(){
             
             
             )   
-          }
-          
-        </div>
+               
         
-
-        
-        )
   }
 }
